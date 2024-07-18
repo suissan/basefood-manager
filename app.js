3b8186c984639e2b134ce2c15660af40259b5c0b
suissan <programming0607@gmail.com>
2024-07-18 21:58:01 +0900

    mysqlのルーティング

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mysql = require("mysql");
 
 const indexRouter = require('./routes/index');
 
@@ -11,6 +12,23 @@ const manageNumberRouter = require('./routes/manage-number');
 //const myInfoRouter = require('./routes/my-info.js');
 
 const app = express();
+
+// 使用するデータベースの情報を記述
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: "u6e67u958b",
+  database: 'base'
+});
+
+// データ・ベース接続時のログ
+connection.connect((err) => {
+  if (err) {
+    console.log(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log('success!');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,12 +50,12 @@ app.use('/products/add', manageNumberRouter);
 //app.use('/get-my-info', myInfoRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -48,3 +66,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+module.exports = connection;
