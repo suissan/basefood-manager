8182eed04559cf06617a98ef56237ce7cfaba0a7
suissan <programming0607@gmail.com>
2024-07-20 00:44:31 +0900

    データベースに情報を登録できるように実装、同期処理のハンドリングも実装

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,65 @@ const connection = require("../db");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const sql = "SELECT * FROM products_stocks";
+  const sql = "SELECT name, stock FROM products_stocks";
   connection.query(sql, (err, results) => {
+    res.render('index', { products: results });
     console.log(results);
   });
-  res.render('index');
 });
 
 // 個数を取得し表示する
-router.get('/get-my-info', function (req, res, next) {
-  baseManager.getBaseInfo()
-    .then((data) => {
-      console.log(data[0]);
-      res.render('index', { products: data });
-    });
+router.get('/add-products', async function (req, res, next) {
+  const selectSql = "SELECT name FROM products_stocks WHERE name = ?";
+  const insertSql = "INSERT INTO products_stocks (name, stock) VALUES ?";
+  const updateSql = "UPDATE products_stocks SET stock = ? WHERE name = ?";
+  
+  try {
+    const products = await baseManager.getBaseInfo();
+    
+    for (const product of products) {
+      const results = await new Promise((resolve, reject) => {
+        connection.query(selectSql, [product.name], (err, results) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        });
+      });
+
+      if (results.length === 0) {
+        const value = [[product.name, product.number]];
+        await new Promise((resolve, reject) => {
+          connection.query(insertSql, [value], (err) => {
+            if (err) {
+              console.log(err);
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        });
+      } else {
+        await new Promise((resolve, reject) => {
+          connection.query(updateSql, [product.number, product.name], (err, results) => {
+            if (err) {
+              console.log(err);
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        });
+      }
+    }
+
+    res.redirect("/");
+  } catch (error) {
+    // エラーハンドリング
+    res.status(500).send("エラーが発生しました");
+  }
 });
 
 module.exports = router;
