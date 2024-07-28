1b26b081fa907c36a102d795777cf72a096bab0b
suissan <programming0607@gmail.com>
2024-07-28 23:41:48 +0900

    データベースの修正

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-    'postgres://suimox7:postgres@localhost/base'
+    process.env.DATABASE_URL || 'postgres://suimox7:postgres@localhost/base'
 );
 
 module.exports = {
